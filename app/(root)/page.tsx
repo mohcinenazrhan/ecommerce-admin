b8d1ed6172bceaedc87f81ee3d393b502dc7a262
Mohcine NAZRHAN <nazrhan.mohcine@gmail.com>
2023-11-17 11:19:41 +0100

    fix: global modal

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,17 +1,25 @@
 "use client";
 
-import { Modal } from "@/components/ui/modal";
-import { UserButton } from "@clerk/nextjs";
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { useEffect } from "react";
 
 export default function SetupPage() {
-const onOpen =   
-const onOpen = 
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+
   return (
     <div className="p-4">
       {/* <UserButton afterSignOutUrl="/" /> */}
       {/* <Modal isOpen onClose={() => {}} title="Test" description="Test Desc">
         Children
       </Modal> */}
+      Root page
     </div>
   );
 }
